9c6a2f0b52e382d40f81fcac1fa0a46f13592a94
KokiSato008800 <kouki00800@gmail.com>
2023-02-06 17:11:17 +0900

    data_uploader完成

--- lib/controllers/question_paper/data_uploader.dart
+++ lib/controllers/question_paper/data_uploader.dart
@@ -3,6 +3,7 @@ import 'dart:convert';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_study_app/firebase_ref/loading_status.dart';
 import 'package:get/get.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_study_app/models/question_paper_model.dart';
@@ -15,26 +16,30 @@ class DataUploader extends GetxController {
     super.onReady();
   }
 
+  final loadingStatus = LoadingStatus.loading.obs; //loadingStatus is obs
+
   Future<void> uploadData() async {
+    loadingStatus.value = LoadingStatus.loading; //0
     // await Firebase.initializeApp();
-    final fireStore = FirebaseFirestore.instance;
+    final firestore = FirebaseFirestore.instance;
     final manifestContent = await DefaultAssetBundle.of(Get.context!)
-      .loadString("AssetManifest.json");
+        .loadString("AssetManifest.json");
     final Map<String, dynamic> manifestMap = json.decode(manifestContent);
     // load json file and print path
     final papersInAssets = manifestMap.keys
-      .where((path) => 
-        path.startsWith("assets/DB/paper")&&path.contains(".json"))
-      .toList();
+        .where((path) =>
+            path.startsWith("assets/DB/paper") && path.contains(".json"))
+        .toList();
     List<QuestionPaperModel> questionPapers = [];
-    for(var paper in papersInAssets) {
+    for (var paper in papersInAssets) {
       String stringPaperContent = await rootBundle.loadString(paper);
-      questionPapers.add(QuestionPaperModel.fromJson(json.decode(stringPaperContent)));
+      questionPapers
+          .add(QuestionPaperModel.fromJson(json.decode(stringPaperContent)));
     }
     // print('Items number ${questionPapers[0].description}');
     var batch = fireStore.batch();
 
-    for( var paper in questionPapers) {
+    for (var paper in questionPapers) {
       batch.set(questionPaperRF.doc(paper.id), {
         "title": paper.title,
         "image_url": paper.imageUrl,
@@ -42,7 +47,22 @@ class DataUploader extends GetxController {
         "time_seconds": paper.timeSeconds,
         "questions_count": paper.questions == null ? 0 : paper.questions!.length
       });
+
+      for (var questions in paper.questions!) {
+        final questionPath =
+            questionRF(paperId: paper.id, questionId: questions.id);
+        batch.set(questionPath, {
+          "question": questions.question,
+          "correct_answer": questions.correctAnswer
+        });
+
+        for (var answer in questions.answers) {
+          batch.set(questionPath.collection("answers").doc(answer.identifier),
+              {"identifier": answer.identifier, "answer": answer.answer});
+        }
+      }
     }
-    return await batch.commit();
+    await batch.commit();
+    loadingStatus.value = LoadingStatus.completed;
   }
 }
--- lib/data_uploader_screen.dart
+++ lib/data_uploader_screen.dart
@@ -2,16 +2,21 @@ import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter_study_app/controllers/question_paper/data_uploader.dart';
+import 'package:flutter_study_app/firebase_ref/loading_status.dart';
 import 'package:get/get.dart';
+
+
 
 class DataUploaderScreen extends StatelessWidget {
   DataUploaderScreen({Key? key}) : super(key: key);
   DataUploader controller = Get.put(DataUploader());
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body:Center(
-        child: Text("uploading"),
+        child: Obx(()=>Text(controller.loadingStatus.value == LoadingStatus.completed
+          ? "Uploading completed"
+          : "Uploading...")),
       ));
   }
 }
